f7ad7fce4075766caa553913eda36154ee0ad923
Johan505 <ajohanmauricio@gmail.com>
2023-02-04 16:17:16 -0500

    first commit

---
+++ src/main/java/com/practica/demosbya0223/Demosbya0223Application.java
@@ -0,0 +1,13 @@
+package com.practica.demosbya0223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demosbya0223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demosbya0223Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/demosbya0223/Demosbya0223ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.demosbya0223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosbya0223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
